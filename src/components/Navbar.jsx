0365d069259d202d2e4246fb63a38b67c4034fe4
DFA1505 <daniel.f.araujo1505@gmail.com>
2026-01-31 09:35:04 -0300

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
   const navLinks = [
     { name: "Home", path: "/" },
     { name: "Hotels", path: "/rooms" },
-    { name: "Experience", path: "/#experience" },
+    { name: "Experience", path: "/" },
     { name: "About", path: "/" },
   ];
 
